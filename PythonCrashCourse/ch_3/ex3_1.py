3a8f779d454a747c00801acc5a87678d573f707b
alliejee <grayskyze@protonmail.com>
2020-03-30 19:35:37 -0500

    added folders and completed first set of ch 3 exercises

---
+++ PythonCrashCourse/ch_3/ex3_1.py
@@ -0,0 +1,15 @@
+friend_names = ['Louise', 'Brittany', 'Faith']
+
+print(friend_names[0])
+print(friend_names[1])
+print(friend_names[2])
+
+# ex3.2
+
+message0 = f"You've got a friend in me {friend_names[0]}"
+message1 = f"You've got a friend in me {friend_names[1]}"
+message2 = f"You've got a friend in me {friend_names[2]}"
+
+print(message0)
+print(message1)
+print(message2)
