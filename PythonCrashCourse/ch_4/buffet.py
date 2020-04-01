1addb08d1b5342d08ac463e2a8347a0ff0b3467a
alliejee <grayskyze@protonmail.com>
2020-04-01 18:05:36 -0500

    finished chapter 4

---
+++ PythonCrashCourse/ch_4/buffet.py
@@ -0,0 +1,11 @@
+menu = ("roast", "steak", "chicken pot pie", "cheeseburger", "spaghetti")
+
+for food in menu:
+    print(food)
+    
+# menu[1] = porkchop
+
+menu = ("salad", "turkey", "chicken pot pie", "cheeseburger", "spaghetti")
+
+for food in menu:
+    print(food)
