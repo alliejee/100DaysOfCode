9423f5b98fe6c533825b24555f686690321f74c3
alliejee <grayskyze@protonmail.com>
2020-04-04 22:27:30 -0500

    added ch 7 exercises

---
+++ PythonCrashCourse/ch_7/restaurant_seating.py
@@ -0,0 +1,8 @@
+number = input("How many people are in your party? ")
+
+number = int(number)
+
+if number > 8:
+    print("You will have to wait for your table.")
+else:
+    print("Your table is ready!")
