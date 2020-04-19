0946503a40dfc118b64453ab96ae2984e77cf4d1
alliejee <grayskyze@protonmail.com>
2020-04-19 18:47:59 -0500

    finished web scraping script

--- RealPython/WebScraper/web_scraper.py
+++ RealPython/WebScraper/web_scraper.py
@@ -6,6 +6,22 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(div='summary')
+results = soup.find(id='ResultsContainer')
 
-print(soup.prettify())
+#print(results.prettify())
+
+#job_elems = results.find_all('section', class_='card-content')
+pen_jobs = results.find_all('h2', string=lambda text: 'penetration' in text.lower())
+for job in pen_jobs:
+    # title_elem = job.find('h2', class_='title')
+    # company_elem = job.find('div', class_='company')
+    # location_elem = job.find('div', class_='location')
+    # if None in (title_elem, company_elem, location_elem):
+    #     continue
+    # print(title_elem.text.strip())
+    # print(company_elem.text.strip())
+    # print(location_elem.text.strip())
+    # print()
+    link = job.find('a')['href']
+    print(job.text.strip())
+    print(f"Apply here: {link}\n")
