f623c2947141b66f3b4bef114f47a8b4f34d3267
alliejee <grayskyze@protonmail.com>
2020-04-28 23:52:04 -0500

    started ssh client

---
+++ ViolentPython/ssh_client.py
@@ -0,0 +1,10 @@
+import paramiko
+import getpass
+
+def main():
+    username = input("username: ")
+    pword = input("password: ")
+    target = input("host: ")
+    
+if __name__ == '__main__':
+    main()
