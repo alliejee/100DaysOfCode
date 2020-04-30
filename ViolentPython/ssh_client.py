532f6b39bfc13c19ee200e5a1af71dd8eb59d369
alliejee <grayskyze@protonmail.com>
2020-04-29 21:44:40 -0500

    added to ssh script

--- ViolentPython/ssh_client.py
+++ ViolentPython/ssh_client.py
@@ -1,10 +1,23 @@
 import paramiko
 import getpass
 
+PORT = 22
+
+def run_command(username, password, cmd, target, port=PORT):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(target, port, username, password)
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    print(stdout.read())
+
+
+
 def main():
     username = input("username: ")
-    pword = input("password: ")
+    pword = getpass.getpass(prompt="password: "))
     target = input("host: ")
+    cmd = 'ls -l /dev'
     
 if __name__ == '__main__':
     main()
